b1833b26080a8abe76f766f1b307e1fddf369532
Tom Povey <povey@Toms-MacBook-Pro.local>
2022-06-28 10:06:13 +0100

    updating directory page with directory component and category item component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import CategoryItem from './components/category-item/category-item.component';
-
-import './categories.styles.scss';
+import Directory from './components/directory/directory.component.jsx'
 
 const App = () => {
   const categories = [
@@ -33,12 +31,11 @@ const App = () => {
   ];
 
   return (
-    <div className='categories-container'>
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
-      ))}
-    </div>
-  );
+    <Directory categories={categories}/>
+  )
+
 };
+
+
 
 export default App;
