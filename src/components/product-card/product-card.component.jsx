fe9ebb04407fbeae046a80b8dbe67c9c516439c3
Tom Povey <povey@Toms-MacBook-Pro.local>
2022-07-22 16:54:11 +0100

    added chakra-ui to shop page

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,4 +1,4 @@
-import { Image, Text, Grid, GridItem } from '@chakra-ui/react';
+import { Image, Text, Box, AspectRatio } from '@chakra-ui/react';
 import Button from '../button/button.component';
 
 
@@ -6,15 +6,16 @@ const ProductCard = ({ product }) => {
     const { name, price, imageUrl } = product;
 
     return (
-        <Grid templateColumns='repeat(5, 1fr)' gap={6} ml={3}>
-           
-            <GridItem mb={4}>
-               <Image src={imageUrl} alt={`${name}`} />
-               <Text fontSize='sm'>{name}</Text>
-               <Text fontSize='sm'>£{price}</Text> 
-               <Button buttonType={'inverted'}>Add to basket</Button>
-            </GridItem>
-        </Grid>
+
+        <Box mt={4} mb={4} w='300px' justify='center'>
+            <AspectRatio maxW='560px' ratio={1}>
+               <Image src={imageUrl} rounded='3xl' objectFit='cover' alt={`${name}`} />
+            </AspectRatio> 
+               <Text fontSize='sm'> {name}</Text> 
+               <Text fontSize='sm'> £{price}</Text>
+               <Button buttonType={'inverted'}>Add to basket</Button>  
+        </Box>
+
 )};
 
 export default ProductCard;
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import { Grid } from '@chakra-ui/react';
 import { ProductsContext } from '../../contexts/products.context';
 
 import ProductCard from '../../components/product-card/product-card.component';
@@ -7,12 +8,17 @@ import ProductCard from '../../components/product-card/product-card.component';
 const Shop = () => {
     const { products } = useContext(ProductsContext);
     return (
-        <div>
+        <Grid 
+        templateColumns={{ base: "repeat(2, 1fr)", lg: "repeat(5, 2fr)" }}
+        columnGap={{ base: "16px", lg: "24px" }}
+        justifyItems="center"
+        padding={{ base: "0 16px" }}
+        paddingTop="30px"
+        >
             {products.map(( product ) => ( 
-                  <ProductCard key={product.id} product={product}/> 
-        
+                 <ProductCard key={product.id} product={product}/>
             ))}
-        </div>
+        </Grid>
     )
 }
 
